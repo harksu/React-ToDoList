eb93df93876cbbcbb04add435d636fcbaed62504
harksu <harksulim@naver.com>
2022-06-24 14:40:28 +0900

    first revise, optional chaining

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import TodoListitem from './TodoListitem'
-import '../styles/TodoList.scss'
+import React from "react";
+import TodoListitem from "./TodoListitem";
+import "../styles/TodoList.scss";
 
 const ToDoList = ({ todos, onRemove }) => {
-    return (
-        <div className='TodoList'>
-            {todos.map(todo => ( // 컴포넌트를 반환하는데, 맵핑을 통해서 해당 컴포넌트에 객체에 대한 정보를 프랍스로 넘김, 삭제 함수도 넘기고.
-                <TodoListitem todo={todo} key={todo.id} onRemove={onRemove} />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="TodoList">
+      {todos?.map(
+        (
+          todo // 컴포넌트를 반환하는데, 맵핑을 통해서 해당 컴포넌트에 객체에 대한 정보를 프랍스로 넘김, 삭제 함수도 넘기고.
+          // + 피드백 반영, optional chaining -> undefined 방어(습관화 하자)
+        ) => (
+          <TodoListitem todo={todo} key={todo.id} onRemove={onRemove} />
+        )
+      )}
+    </div>
+  );
+};
 
-export default ToDoList
+export default ToDoList;
